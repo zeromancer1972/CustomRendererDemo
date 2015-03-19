a344704ddd2ce2fbe815153aa5c781e7b0963065
zeromancer1972 <obusse@googlemail.com>
2015-03-19 03:23:57 +0100

    Welcome screen

--- ODP/Code/Java/org/openntf/uikit/Page.java
+++ ODP/Code/Java/org/openntf/uikit/Page.java
@@ -7,7 +7,6 @@ public class Page implements Serializable {
 	private static final long serialVersionUID = 45923476176461869L;
 	private final String label;
 	private final String url;
-	private boolean active;
 
 	public Page(final String label, final String url) {
 		this.label = label;
